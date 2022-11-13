497682ab6c61be309b6323759ee805625490141a
bramvar <36579511+bramvar@users.noreply.github.com>
2022-11-13 17:54:01 -0500

    add console message

--- index.ts
+++ index.ts
@@ -4,3 +4,4 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+console.log('estoy cansando carajo');
